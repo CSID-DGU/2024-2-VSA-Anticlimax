d165eabe4b9297951d84022a6fe9914ff8a6aca9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-12-22 14:38:30 +0900

    feat : init project

---
+++ next.config.ts
@@ -0,0 +1,49 @@
+import { NextConfig } from 'next';
+import { Configuration } from 'webpack';
+
+type ResourceQuery = { not?: (string | RegExp)[] };
+
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [process.env.NEXT_PUBLIC_S3_BUCKET_URL || ''],
+    },
+    webpack(config: Configuration) {
+        const fileLoaderRule = config.module?.rules?.find((rule) => {
+            return (
+                rule &&
+                typeof rule !== 'string' &&
+                rule.test instanceof RegExp &&
+                rule.test.test('.svg')
+            );
+        });
+
+        if (fileLoaderRule && typeof fileLoaderRule !== 'string') {
+            config.module?.rules?.push(
+                {
+                    ...fileLoaderRule,
+                    test: /\.svg$/i,
+                    resourceQuery: /url/,
+                },
+                {
+                    test: /\.svg$/i,
+                    issuer: fileLoaderRule.issuer,
+                    resourceQuery: {
+                        not: [
+                            ...((fileLoaderRule.resourceQuery as ResourceQuery)
+                                ?.not || []),
+                            /url/,
+                        ],
+                    },
+                    use: ['@svgr/webpack'],
+                },
+            );
+
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
+        return config;
+    },
+};
+
+export default nextConfig;
