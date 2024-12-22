9cf56539e5903335f409253dcee12d5a526db70f
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-12-22 14:49:10 +0900

    feat: image, svg assets

---
+++ public/images/index.ts
@@ -0,0 +1,6 @@
+import Banner1 from './banner_1.png';
+import Banner2 from './banner_2.png';
+import DefaultDrugImage from './img-drug.png';
+import DefaultVitaminImage from './img-vitamin.png';
+
+export { Banner1, Banner2, DefaultDrugImage, DefaultVitaminImage };
---
+++ public/svgs/index.ts
@@ -0,0 +1,47 @@
+import IconActiveCheckbox from './active-checkbox.svg';
+import IconAndroid from './android.svg';
+import IconApple from './apple.svg';
+import IconChevronDown from './chevron-down.svg';
+import IconChevronLeft from './chevron-left.svg';
+import IconClose from './close.svg';
+import IconHamburger from './hamburger.svg';
+import IconInActiveCheckbox from './in-active-checkbox.svg';
+import IconInstagram from './instagram.svg';
+import IconKakao from './kakao.svg';
+import IconLeftChevron from './left-chevron.svg';
+import IconPlus from './plus.svg';
+import IconQRAndroid from './qr-code-android.svg';
+import IconQR from './qr-code-ios.svg';
+import IconRightChevron from './right-chevron.svg';
+import IconSearch from './search.svg';
+import IconSearchDrug from './search-drug.svg';
+import IconStar from './star.svg';
+import ViewpamLogo from './viewpam.svg';
+import WooahanDownloadLogo from './wooahan-download-logo.svg';
+import WooahanNavLogo from './wooahan-nav-logo.svg';
+import IconXRounded from './x-rounded.svg';
+
+export {
+    IconActiveCheckbox,
+    IconAndroid,
+    IconApple,
+    IconChevronDown,
+    IconChevronLeft,
+    IconClose,
+    IconHamburger,
+    IconInActiveCheckbox,
+    IconInstagram,
+    IconKakao,
+    IconLeftChevron,
+    IconPlus,
+    IconQR,
+    IconQRAndroid,
+    IconRightChevron,
+    IconSearch,
+    IconSearchDrug,
+    IconStar,
+    IconXRounded,
+    ViewpamLogo,
+    WooahanDownloadLogo,
+    WooahanNavLogo,
+};
